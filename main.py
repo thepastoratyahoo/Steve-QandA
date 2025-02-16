72ab6fcd4d2fb8d4bfe65f4d8151aac8238bd8d7
Pastor Steve <39284015-steve374@users.noreply.replit.com>
2025-02-16 02:18:20 +0000

    Initial commit

--- main.py
+++ main.py
@@ -2,5 +2,12 @@ name = input("What is your name? ")
 verse = input("What is your favorite verse? ")
 challenge = input("What is a challenge you face? ")
 friend = input("Who is a friend that might help you? ")
-print ()
-print("Hello " + "\033[34m" + name + "\033[0m" + ", I hope you are doing well. I am here to tell you about your favorite verse, " + "\033[33m" + verse + "\033[0m" + ". I hope you have memorized it. It can empower you to overcome your challenge, " + "\033[31m" + challenge + "\033[0m" + ". I hope you can find someone to help you. " + "\033[32m" + friend + "\033[0m" + " might be wise to help you. Reach out to them today!")
+print()
+print(
+    "Hello " + "\033[34m" + name + "\033[0m" +
+    ", I hope you are doing well. I am here to tell you about your favorite verse, "
+    + "\033[33m" + verse + "\033[0m" +
+    ". I hope you have memorized it. It can empower you to overcome your challenge, "
+    + "\033[31m" + challenge + "\033[0m" +
+    ". I hope you can find someone to help you. " + "\033[32m" + friend +
+    "\033[0m" + " might be wise to help you. Reach out to them today!")
